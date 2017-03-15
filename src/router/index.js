5782f666efd26b572ad25be75bf1ed0a42ae0d9c
Tim Hayes <tim@thetravelyear.com>
2017-03-14 22:13:04 -0400

    Modified scroll behavior
    Will now return to saved scroll position on previous page

--- src/router/index.js
+++ src/router/index.js
@@ -23,5 +23,12 @@ export default new Router({
       name: 'Contact',
       component: Contact
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
