76fd600a2e0c5011fbc89dac74a31c124b87f93c
Tim Hayes <tim@thetravelyear.com>
2017-03-25 22:58:30 -0400

    Added CSS themes for stories

--- src/components/Story.vue
+++ src/components/Story.vue
@@ -45,5 +45,234 @@ export default {
 
 <style scoped>
 
+.slides {
+  height: 90vh;
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
+
+.slide {
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  background-size: contain;
+  display: block;
+  font-size: 1.5rem;
+  height: 100%;
+  overflow: auto;
+  position: absolute;
+  transition: left 0.5s ease-in-out;
+  width: 100%;
+}
+  .slide.before {
+    left: -100%
+  }
+  .slide.current {
+    left: 0;
+  }
+  .slide.after {
+    left: 100%
+  }
+
+.slide-contents {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  min-height: 100%;
+  opacity: 0;
+  transition: opacity 1s 0.5s;
+}
+  .slide.current .slide-contents {
+    opacity: 1;
+  }
+
+.btn-nav {
+  background: #fff;
+  background-position: center center;
+  background-repeat: no-repeat;
+  border: 5px black solid;
+  border-radius: 25px;
+  bottom: 10px;
+  height: 50px;
+  width: 50px;
+  position: absolute;
+  transition: opacity 0.5s;
+}
+  .btn-nav.hidden {
+    opacity: 0;
+  }
+  .btn-nav:focus {
+    outline: 0;
+    border-color: red;
+  }
+  #back {
+    background-image: url('/static/media/backarrow.png');
+    left: 20px;
+  }
+  #next {
+    background-image: url('/static/media/nextarrow.png');
+    right: 20px;
+  }
+
+/* slide theme colors */
+
+
+.buttercup {
+  background-color: #f39c12;
+}
+
+.chambray {
+  background-color: #3a539b;
+  color: white;
+}
+
+.eucalyptus {
+  background-color: #26a65b;
+}
+
+.malibu {
+  background-color: #1bbaf2;
+}
+
+.pumice {
+  background-color: #d2d7d3;
+}
+
+</style>
+
+<style>
+
+/* Non-scoped classes */
+
+@keyframes zoomAnimation {
+  0% {
+    transform: scale(0);
+  }
+  80% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+/* template: intro */
+
+  .slide.intro .slide-root {
+    animation: 0.5s 0.5s ease-in-out zoomAnimation forwards;
+    border: 10px solid red;
+    border-radius: 100%;
+    padding: 50px 110px;
+    transform: scale(0);
+  }
+
+  .slide.intro h1 {
+    font-size: 2.5rem;
+    font-family: 'Changa One', cursive;
+    text-align: center;
+  }
+
+/* template: callout */
+
+  .slide.callout > div {
+    font-size: 1.75em;
+    padding: 0 10%;
+    text-align: center;
+  }
+
+/* template: flex-rows */
+
+  .slide.flex-rows .slide-root {
+    padding: 20px;
+  }
+
+  .slide.flex-rows .slide-root > div {
+    margin: 10px 0;
+  }
+  .slide.flex-rows .slide-root > div:first-child {
+    margin-top: 0;
+  }
+  .slide.flex-rows .slide-root > div:last-child {
+    margin-bottom: 0;
+  }
+
+  .slide.flex-rows img {
+    display: block;
+    margin: 0 auto;
+    max-width: 100%;
+  }
+
+/* template: flex-columns */
+
+  .slide.flex-columns .slide-root {
+    flex: 1;
+    justify-content: center;
+    padding: 20px;
+  }
+
+  .slide.flex-columns img {
+    display: block;
+    margin: 0 auto;
+    max-width: 100%;
+  }
+
+
+/* generic slide elements */
+
+  .slide p {
+    padding-bottom: 1em;
+  }
+
+  .slide.content-padded .slide-root {
+    padding: 20px;
+  }
+
+  .slide.bold {
+    font-weight: bold;
+  }
+
+  .slide .img-panel {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 10px;
+  }
+    .slide .img-panel > div, .slide .img-panel img {
+      width: 100%;
+    }
+
+  @media screen and (min-width: 768px) {
+
+    .slide.flex-columns .slide-root {
+      align-items: center;
+      display: flex;
+    }
+    .slide.flex-columns .slide-root > div {
+      flex: 1;
+    }
+    .slide.flex-columns .slide-root > div {
+      margin: 0 10px;
+    }
+    .slide.flex-columns .slide-root > div:first-child {
+      margin-left: 0;
+    }
+    .slide.flex-columns .slide-root > div:last-child {
+      margin-right: 0;
+    }
+
+    .slide .img-panel {
+      flex-direction: row;
+    }
+    .slide .img-panel > * {
+      padding: 0 10px
+    }
+    .slide .img-panel > *:first-child {
+      padding-left: 0;
+    }
+    .slide .img-panel > *:last-child {
+      padding-right: 0;
+    }
+
+  }
 
 </style>
