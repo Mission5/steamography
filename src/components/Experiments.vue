dafa2147a719ab6c8011fe4c092d82a4bc87ef0c
Tim Hayes <tim@thetravelyear.com>
2017-03-27 22:39:18 -0400

    Added new content to experiments
    Includes new links, disclaimer, style updates, and text edits

--- src/components/Experiments.vue
+++ src/components/Experiments.vue
@@ -14,6 +14,12 @@
         <div><a class="btn" v-bind:href="activity.href" v-bind:class="activity.type" target="blank">Go</a></div>
       </li>
     </ul>
+    <div class="disclaimer">Disclaimer: The STEAM Activities section may contain links to Web sites created and
+      maintained by other public and/or private organizations. We provide links to these sites as a service to
+      our users. The presence of a link is not an endorsement of STEAMography or Leland Melvin. When users follow
+      a link to an outside Web site, they are leaving STEAMography and are subject to the privacy and security
+      policies of the owners/sponsors of the outside Web site(s). We are not responsible for the information
+      collection practices of any other site.
   </div>
 </template>
 
@@ -39,20 +45,30 @@ export default {
           href: '/static/resources/BuildAPaperRocketWeb.pdf',
           type: 'pdf'
         }, {
-          title: 'Build A Water Bottle Rocket Assembly',
+          title: 'Build a Water Bottle Rocket Assembly',
           description: 'In this activity you will build a more advanced Water Bottle Rocket, test your rocket for stability and learn how to assemble a rocket launcher.',
           href: '/static/resources/BuildAWaterBottleRocketAssemblyWeb.pdf',
+          type: 'pdf'
+        }, {
+          title: 'From the Hubble',
+          description: 'If your students enjoyed the Great Big Story videos about Leland\'s experiences in space, check out the The NASA Hubblecast. The site presents short informative videos that introduce viewers to lessons in astronomy, telescopes, space facts and the beautiful imagery from the Hubble telescope.',
+          href: 'http://www.spacetelescope.org/videos/archive/category/hubblecast/',
           type: 'external'
         }, {
-          title: 'Activity 5',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quam, porro hic architecto enim assumenda molestiae magnam',
-          href: '#',
-          type: 'internal'
+          title: 'Public Opinion Poll',
+          description: 'As an astronaut, Leland has been interviewed many times about the value of space programs. He continues to be an advocate for exploration, space education and research. This activity introduces students to the basics ideas behind creating a survey, gathering data, and analyzing answers to determine public opinion about the value of space exploration to our society.',
+          href: 'http://phoenix.lpl.arizona.edu/pdf/lesson_5.pdf',
+          type: 'pdf'
         }, {
-          title: 'Activity 6',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At quam, porro hic architecto enim assumenda molestiae magnam',
-          href: '#',
-          type: 'document'
+          title: 'Cosmic Colors',
+          description: 'Leland took his photographs from space using a regular camera and a long lens - but many objects in space are not visible to the naked eye. Explore how different types of telescopes help detect what\'s invisible to the human eye and reveal Cosmic Colors.',
+          href: 'https://spaceplace.nasa.gov/cosmic-colors/en/#',
+          type: 'external'
+        }, {
+          title: 'Blind Engineering',
+          description: 'Leland trained for months with his team to perfect his communication while using the robotic arm in space. Precise instructions and specific responses are needed to prevent mission failure and to keep everyone alive! This activity provides the opportunity for students to experience the difficulty of communication in engineering an object using Legos or building blocks.',
+          href: 'http://phoenix.lpl.arizona.edu/pdf/lesson_11.pdf',
+          type: 'pdf'
         }
       ]
     }
@@ -63,18 +79,28 @@ export default {
 <style scoped>
 
   .activity {
-    padding: 1rem;
+    padding: 1rem 1rem 1rem 0;
     -webkit-column-break-inside: avoid;
           page-break-inside: avoid;
                break-inside: avoid;
   }
+
   .btn {
     margin-top: 5px;
+  }
+
+  .disclaimer {
+    border-top: 1px #ddd solid;
+    font-size: 0.9rem;
+    padding-top: 1rem;
   }
   
   @media screen and (min-width: 768px) {
     .activities {
       column-count: 2;
+    }
+    .activity {
+      display: inline-block;
     }
   }
 
