a5b8adb734ac3ed95909c0f5a78a2f8ab5958b29
Tim Hayes <tim@thetravelyear.com>
2017-04-10 15:02:11 -0400

    Animation iPhone Fix 2
    Still troubleshooting issues

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="animation">
-    <iframe src="/static/media/animation/index.html"></iframe>
+  <div class="animation" style="padding-bottom: 56.25%; position: relative; overflow: hidden;">
+    <iframe src="/static/media/animation/index.html" 
+      style="border: 0; position: absolute; height: 100%; width: 100%;"></iframe>
   </div>
 </template>
 
@@ -10,19 +11,3 @@ export default {
   name: 'animation'
 }
 </script>
-
-<style scoped>
-
-  .animation {
-    padding-bottom: 56.25%;
-    position: relative;
-    overflow: hidden;
-  }
-
-  iframe {
-    border: 0;
-    position: absolute;
-    height: 100%;
-    width: 100%;
-  }
-</style>
