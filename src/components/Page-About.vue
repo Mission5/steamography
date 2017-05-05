16d79f873be40fca8165e04880a4aa8016fa0754
Tim Hayes <tim@thetravelyear.com>
2017-05-04 21:33:21 -0400

    Roughed out About text
    Issue #45 - no graphics added; needs work

--- src/components/Page-About.vue
+++ src/components/Page-About.vue
@@ -1,7 +1,51 @@
 
 <template>
   <main class="box">
-    About page content needed.
+
+  <div class="text-center">
+  <p><b>S.T.E.A.M.</b> Science . Technology . Engineering . Arts . Math</p>
+  <p><b>OGRAPHY</b> Writing . Representation . Descriptive . Visual . Story</p>
+  <p>The power of S.T.E.A.M. driven by the power of STORY.</p>
+</div>
+  <hr/>
+
+  <p>
+    Life stories inspire and connect us to each other.  And so many amazing life stories have been
+    shaped by S.T.E.A.M. – like that of Leland Melvin.  Working in partnership with Leland, a retired
+    astronaut and former NFL wide receiver, our website launches in conjunction with his memoir,
+    Chasing Space.  We are proud to present Leland’s unique story as our first STEAMography.
+  </p>
+
+  <p class="text-center">Transformative. Interactive. Personal.</p>
+
+  <p>
+    STEAMography encourages learning as exploration. Interactive stories reveal the influential role
+    that STEAM-oriented education has played in Leland’s life. These real-life moments provide a way
+    for kids to engage with S.T.E.A.M. in an inspirational context.  A strong portfolio of STEAMographies
+    will help spark, develop, and reinforce their interest in S.T.E.A.M. topics.
+  </p>
+
+  <p>
+    All of learning takes place in the midst of our life story – and the story of a life well lived
+    inspires us all to keep learning.
+  </p>
+
+  <hr/>
+
+  <p>
+    We are a dedicated group of ragtag misfits - artists, programmers, animators, poets, gardeners,
+    adventurers, astronauts, geeks and foodies - who believe passionately that we can change the world –
+    one STEAMography at a time.
+  </p>
+
+  <ul>
+    <li>Michelle Wee</li>
+    <li>Stan Webb</li>
+    <li>Tony Mancus</li>
+    <li>Tim Hayes</li>
+    <li>Melissa Wallace</li>
+    <li>Leland Melvin</li>
+  </ul>
   </main>
 </template>
 
@@ -15,5 +59,7 @@ export default {
 </script>
 
 <style scoped>
-
+  p { 
+    margin: 1rem 0;
+  }
 </style>
