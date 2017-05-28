2a229cfe3b65233a48325dd80a336c762cdc4e0b
Tim Hayes <tim@thetravelyear.com>
2017-05-28 10:41:23 -0400

    Modal scroll improvements
    Handle scrolling with modal windows more appropriately on mobile devices with hidden scrollbars

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -12,20 +12,44 @@
 </template>
 
 <script>
-export default {
-  name: 'modal',
-  props: {
-    'width': {
-      default: '600px'
+  export default {
+    name: 'modal',
+    props: {
+      'width': {
+        default: '600px'
+      }
+    },
+    created () {
+      if (windowHasScrollbar()) document.body.classList.add('modal-open-scroll')
+      document.body.classList.add('modal-open')
+    },
+    destroyed () {
+      document.body.classList.remove('modal-open', 'modal-open-scroll')
     }
-  },
-  created () {
-    document.body.classList.add('modal-open')
-  },
-  destroyed () {
-    document.body.classList.remove('modal-open')
   }
-}
+
+  var windowHasScrollbar = function () {
+    // The Modern solution
+    if (typeof window.innerWidth === 'number') return window.innerWidth > document.documentElement.clientWidth
+
+    // rootElem for quirksmode
+    var rootElem = document.documentElement || document.body
+
+    // Check overflow style property on body for fauxscrollbars
+    var overflowStyle
+    if (typeof rootElem.currentStyle !== 'undefined') overflowStyle = rootElem.currentStyle.overflow
+    overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow
+
+    // Also need to check the Y axis overflow
+    var overflowYStyle
+    if (typeof rootElem.currentStyle !== 'undefined') overflowYStyle = rootElem.currentStyle.overflowY
+    overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY
+    var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight
+    var overflowShown = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle)
+    var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll'
+    return (contentOverflows && overflowShown) || (alwaysShowScroll)
+  }
+
 </script>
 
 <style scoped>
